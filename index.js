816313e6ae3732b3d4f60a98b16f435075825979
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 08:44:07 -0600

    working on tempConvert function in index.js

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ console.log("hello");
 //convert temp button and function
 document.getElementById("convert").onclick = tempConvert;
 
+function tempConvert() {
+  if (farenheit != "") {
+    celcius = (parseFloat(farenheit) - 32) / 1.8;
+  } else {
+    farenheit = parseFloat(celcius) * 1.8 + 32;
+  }
+}
+
 //clear form button and function
 document.getElementById("clear").onclick = clearForm;
 
