99bdc19cf3e7fdb979c03262a76999d421db3da6
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 08:39:55 -0600

    working on clearForm function in index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log('hello');
-//html button action
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear').onclick = clearForm;
+console.log("hello");
+//convert temp button and function
+document.getElementById("convert").onclick = tempConvert;
+
+//clear form button and function
+document.getElementById("clear").onclick = clearForm;
+
+function clearForm() {
+  document.getElementById("farenheit").value = "";
+  document.getElementById("celsius").value = "";
+}
