bc7be12975402dfb5bed14212e931d9aa38fa5dd
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 11:25:51 -0600

    consts farenheit and celcius and if/else statement in index.js

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ console.log("hello");
 document.getElementById('convert').onclick = tempConvert;
 document.getElementById('clear') = clearForm;
 
+function tempConvert() {
+    const farenheit = document.getElementById('farenheit').value;
+    const celcius = document.getElementById('celcius').value;
+
+    if (farenheit != '') {
+        celcius = (parseFloat(farenheit) - 32) / 1.8;
+          
+    } else {
+        farenheit = (parseFloat(celcius) * 1.8) + 32;
+    }
+    
+}
