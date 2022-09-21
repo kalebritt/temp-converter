150d08100a7f63890e5f669f6de32386abb96fc9
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 11:48:18 -0600

    trying to get functions to work

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 console.log("hello");
 //convert temp button and function
 
-document.getElementById('convert').onclick === tempConvert();
-document.getElementById('clear') === clearForm();
+document.getElementById("convert").onclick === tempConvert();
+document.getElementById("clear") === clearForm();
 
 function tempConvert() {
-    var farenheit = document.getElementById('farenheit').value;
-    var celcius = document.getElementById('celcius').value;
+  var farenheit = document.getElementById("farenheit").value;
+  var celcius = document.getElementById("celcius").value;
 
-    if (farenheit != '') {
-        celcius = (parseFloat(farenheit) - 32) / 1.8;
-          
-    } else {
-        farenheit = (parseFloat(celcius) * 1.8) + 32;
-    }
-     
-    document.getElementById('farenheit').value = parseFloat(farenheit).toFixed(1);
-    document.getElementById('celcius').value = parseFloat(celcius).toFixed(1);
-};
+  if (farenheit != "") {
+    celcius = (parseFloat(farenheit) - 32) / 1.8;
+  } else {
+    farenheit = parseFloat(celcius) * 1.8 + 32;
+  }
+
+  document.getElementById("farenheit").value = parseFloat(farenheit).toFixed(1);
+  document.getElementById("celcius").value = parseFloat(celcius).toFixed(1);
+}
 
 function clearForm() {
-    document.getElementById('farenheit').value = '';
-    document.getElementById('celcius').value = '';
-    
-    
-};
+  document.getElementById("farenheit").value = "";
+  document.getElementById("celcius").value = "";
+}
