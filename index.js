31595736b1227a4d011928b81cfd3348425d8e81
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 11:38:53 -0600

    some minor changes in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log("hello");
 //convert temp button and function
 
-document.getElementById('convert').onclick = tempConvert;
-document.getElementById('clear') = clearForm;
+document.getElementById('convert').onclick === tempConvert();
+document.getElementById('clear') === clearForm();
 
 function tempConvert() {
-    const farenheit = document.getElementById('farenheit').value;
-    const celcius = document.getElementById('celcius').value;
+    var farenheit = document.getElementById('farenheit').value;
+    var celcius = document.getElementById('celcius').value;
 
     if (farenheit != '') {
         celcius = (parseFloat(farenheit) - 32) / 1.8;
@@ -14,5 +14,13 @@ function tempConvert() {
     } else {
         farenheit = (parseFloat(celcius) * 1.8) + 32;
     }
+     
+    document.getElementById('farenheit').value = parseFloat(farenheit).toFixed(1);
+    document.getElementById('celcius').value = parseFloat(celcius).toFixed(1);
+};
+
+function clearForm() {
+    document.getElementById('farenheit').value = '';
+    document.getElementById('celcius').value = '';
     
-}
+};
