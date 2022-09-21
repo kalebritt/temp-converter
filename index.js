fda0bfa0ae672735c11dc3033350e671e3a52527
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-09-21 11:21:48 -0600

    started over index.js

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 console.log("hello");
 //convert temp button and function
-document.getElementById("convert").onclick = tempConvert;
 
-function tempConvert() {
-  if (farenheit != "") {
-    celcius = (parseFloat(farenheit) - 32) / 1.8;
-  } else {
-    farenheit = parseFloat(celcius) * 1.8 + 32;
-  }
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('clear') = clearForm;
 
-  document.getElementById("farenheit").toFixed(1);
-  document.getElementById("celcius").value = parseFloat(celcius).toFixed(1);
-}
-//clear form button and function
-document.getElementById("clear").onclick = clearForm;
-
-function clearForm() {
-  document.getElementById("farenheit").value = "";
-  document.getElementById("celcius").value = "";
-}
